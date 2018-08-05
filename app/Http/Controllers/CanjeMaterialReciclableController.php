03df657773d7787362452b31646f012eeb585ec5
dyl-XD@hotmail.com <dyl-XD@hotmail.com>
2018-08-05 13:27:44 -0600

    Agregando Controlador de Canje Material Reciclable

---
+++ app/Http/Controllers/CanjeMaterialReciclableController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CanjeMaterialReciclableController extends Controller
+{
+    //
+
+    public function getIndexCanjeMaterial(){
+        dd('test');
+    }
+
+}
